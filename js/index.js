f619e7435eb4697a4743c6c33376c03a8a7247d5
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-06-05 00:09:55 -0500

    Level 2: Apply styling and add flight data to table

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 "use strict";
 const getJSON = async () => {
-  const data = await fetch("data/flight_logs.json").then((response) =>
+  const data = await fetch("https://my.api.mockaroo.com/flight_logs.json?key=5776e910").then((response) =>
     response.json()
   );
   return data;
 };
+
 
 document.addEventListener("DOMContentLoaded", async () => {
   const myArray = await getJSON();
   const form = document.getElementById("form-id");
 
   form.addEventListener("submit", function (event) {
+    // clears old table before adding a new one
+    const infoDiv = document.getElementById("info");
+    infoDiv.innerHTML = "";
+
     // Prevent page reload
     event.preventDefault();
 
@@ -22,13 +27,39 @@ document.addEventListener("DOMContentLoaded", async () => {
       (flight) => flight.airline == selectedAirline
     );
 
-    let formattedFlights = airlineName.map((flightInfo) => {
-      return `Flight ${flightInfo.flight_number}: Departing from ${flightInfo.departure_airport} to ${flightInfo.arrival_airport} Departure: ${flightInfo.departure_date} at ${flightInfo.departure_time} Arrival: ${flightInfo.arrival_date} at ${flightInfo.arrival_time} Duration: ${flightInfo.flight_duration} hours Passengers: ${flightInfo.passenger_count}`;
+    const table = document.createElement("table");
+    const tableHeader = document.createElement("thead");
+    const tableBody = document.createElement("tbody");
+
+    airlineName.map((flightInfo) => {
+      const row = document.createElement("tr");
+    
+      let cell = document.createElement("td");
+      cell.textContent = flightInfo.flight_number;
+      row.appendChild(cell);
+
+      cell = document.createElement("td");
+      cell.textContent = flightInfo.departure_airport;
+      row.appendChild(cell);
+
+      cell = document.createElement("td");
+      cell.textContent = flightInfo.departure_date;
+      row.appendChild(cell);
+
+      cell = document.createElement("td");
+      cell.textContent = flightInfo.departure_time;
+      row.appendChild(cell);
+
+      tableBody.appendChild(row);
     });
 
-    let finalOutput = formattedFlights.join("\n\n");
-
-    output.textContent = finalOutput;
+    table.appendChild(tableHeader);
+    // put the <tbody> in the <table>
+    table.appendChild(tableBody);
+    // appends <table> into <body>
+    const htmlTable = document.getElementById("info").appendChild(table);
+    // sets the border attribute of table to '2'
+    table.setAttribute("border", "2");
 
     setTimeout(() => {
       this.reset();
