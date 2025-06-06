a0f8fffcd4d03890676d28e7c60815a1f674b026
prettyalana <129349619+prettyalana@users.noreply.github.com>
2025-06-05 19:17:55 -0500

    Add headers to columns and table

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 "use strict";
 const getJSON = async () => {
-  const data = await fetch("https://my.api.mockaroo.com/flight_logs.json?key=5776e910").then((response) =>
-    response.json()
-  );
+  const data = await fetch(
+    "https://my.api.mockaroo.com/flight_logs.json?key=5776e910"
+  ).then((response) => response.json());
   return data;
 };
-
 
 document.addEventListener("DOMContentLoaded", async () => {
   const myArray = await getJSON();
@@ -23,18 +22,50 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     const selectedAirline = document.getElementById("airline-select").value;
 
-    let airlineName = [...myArray].filter(
+    let airlineFlightObjects = [...myArray].filter(
       (flight) => flight.airline == selectedAirline
     );
 
     const table = document.createElement("table");
+    const airlineNameHeader = document.createElement("thead");
     const tableHeader = document.createElement("thead");
     const tableBody = document.createElement("tbody");
 
-    airlineName.map((flightInfo) => {
+    // get the airline name from the airlineFlightObjects array
+    const airlineInfo = airlineFlightObjects[0]["airline"];
+
+    // Create the airline name row
+    const airlineRow = document.createElement("tr");
+
+    // Create the airline name header
+    let airline = document.createElement("th");
+    airline.textContent = airlineInfo;
+    airlineRow.appendChild(airline).colSpan = "4";
+
+    airlineNameHeader.appendChild(airlineRow);
+
+    const columnHeaders = ["Time", "Flight", "Airport", "Date"];
+
+    const headerRow = document.createElement("tr");
+
+    columnHeaders.forEach((headerName) => {
+      let header = document.createElement("th");
+
+      header.textContent = headerName;
+
+      headerRow.appendChild(header);
+    });
+
+    tableHeader.appendChild(headerRow);
+
+    airlineFlightObjects.map((flightInfo) => {
       const row = document.createElement("tr");
-    
+
       let cell = document.createElement("td");
+      cell.textContent = flightInfo.departure_time;
+      row.appendChild(cell);
+
+      cell = document.createElement("td");
       cell.textContent = flightInfo.flight_number;
       row.appendChild(cell);
 
@@ -46,13 +77,10 @@ document.addEventListener("DOMContentLoaded", async () => {
       cell.textContent = flightInfo.departure_date;
       row.appendChild(cell);
 
-      cell = document.createElement("td");
-      cell.textContent = flightInfo.departure_time;
-      row.appendChild(cell);
-
       tableBody.appendChild(row);
     });
 
+    table.appendChild(airlineNameHeader);
     table.appendChild(tableHeader);
     // put the <tbody> in the <table>
     table.appendChild(tableBody);
